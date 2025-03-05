f3d414c4772aaa87101c6cca8d8052b080906a12
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-05 17:41:48 +0100

    fix: windcompass breaking (#22)
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skycubed/features/overlays/WindOverlay.kt
+++ src/client/kotlin/tech/thatgravyboat/skycubed/features/overlays/WindOverlay.kt
@@ -3,12 +3,14 @@ package tech.thatgravyboat.skycubed.features.overlays
 import net.minecraft.network.chat.Component
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyIn
+import tech.thatgravyboat.skyblockapi.api.events.base.predicates.TimePassed
 import tech.thatgravyboat.skyblockapi.api.events.chat.ChatReceivedEvent
 import tech.thatgravyboat.skyblockapi.api.events.hypixel.ServerChangeEvent
 import tech.thatgravyboat.skyblockapi.api.events.render.RenderHudEvent
 import tech.thatgravyboat.skyblockapi.api.events.time.TickEvent
 import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
 import tech.thatgravyboat.skyblockapi.helpers.McClient
+import tech.thatgravyboat.skyblockapi.helpers.McFont
 import tech.thatgravyboat.skyblockapi.utils.text.TextProperties.stripped
 import tech.thatgravyboat.skycubed.config.overlays.OverlaysConfig
 import tech.thatgravyboat.skycubed.utils.pushPop
@@ -16,53 +18,34 @@ import tech.thatgravyboat.skycubed.utils.pushPop
 object WindOverlay {
 
     private var windCompassComponent: Component? = null
-    private var index: Int? = null
-    private var checked = false
 
     @Subscription
     @OnlyIn(SkyBlockIsland.DWARVEN_MINES, SkyBlockIsland.CRYSTAL_HOLLOWS)
     fun onHudRender(event: RenderHudEvent) {
         if (!OverlaysConfig.windOverlay) return
-        if (windCompassComponent != null) {
-
-            val font = McClient.self.font
-
+        windCompassComponent?.let {
             event.graphics.pushPop {
                 translate(
                     (event.graphics.guiWidth() / 2).toFloat(),
                     (event.graphics.guiHeight() / 2).toFloat(),
                     0.0F
-                );
-                scale(1.2F, 1.2F, 1.2F);
+                )
+                scale(1.2F, 1.2F, 1.2F)
 
-                val width = font.width(windCompassComponent!!)
-                event.graphics.drawStringWithBackdrop(font, windCompassComponent!!, -width / 2, -10, width, 0xffffff);
+                val width = McFont.width(it)
+                event.graphics.drawStringWithBackdrop(McFont.self, it, -width / 2, -10, width, 0xffffff)
             }
         }
     }
 
     @Subscription
+    @TimePassed("2t")
     @OnlyIn(SkyBlockIsland.DWARVEN_MINES, SkyBlockIsland.CRYSTAL_HOLLOWS)
-    fun onScoreboardUpdate(event: TickEvent) {
+    fun onTick(event: TickEvent) {
         val scoreboard = McClient.scoreboard.toList()
-        if (index == null && !checked) {
-            scoreboard.forEachIndexed { index, line ->
-                if (line.stripped == "Wind Compass") {
-                    this.index = index + 1
-                    windCompassComponent = scoreboard.getOrNull(this.index!!)
-                    return@forEachIndexed
-                }
-            }
-            checked = true
-        } else if (index != null) {
-            val line = scoreboard.getOrNull(this.index!!)
-            val previousLine = scoreboard.getOrNull(this.index!! - 1)
-            if (previousLine?.stripped == "Wind Compass") {
-                windCompassComponent = line
-            } else {
-                index = null
-            }
-        }
+        val index = scoreboard.indexOfFirst { it.stripped == "Wind Compass" }.takeUnless { it == -1 } ?: return
+
+        windCompassComponent = scoreboard.getOrNull(index + 1)
     }
 
     @Subscription
@@ -70,16 +53,11 @@ object WindOverlay {
     fun onChatMessage(event: ChatReceivedEvent) {
         if (event.text.contains("GONE WITH THE WIND ENDED!")) {
             windCompassComponent = null
-            index = null
-        } else if (event.text.contains("GONE WITH THE WIND STARTED!")) {
-            checked = false
         }
     }
 
     @Subscription
     fun onServerChange(event: ServerChangeEvent) {
         windCompassComponent = null
-        index = null
-        checked = false
     }
 }
