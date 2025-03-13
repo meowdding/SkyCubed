8b8baec71042496c6dfd84ae545866161e8dcd64
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-03-13 19:33:49 +0100

    fix: missing translations (#24)
    * fix: missing translations
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * oops
    
    ---------
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skycubed/config/notifications/NotificationObject.kt
+++ src/client/kotlin/tech/thatgravyboat/skycubed/config/notifications/NotificationObject.kt
@@ -8,18 +8,18 @@ import com.teamresourceful.resourcefulconfig.api.annotations.ConfigOption
 @ConfigObject
 data class NotificationObject(
 
-    @ConfigEntry(id = "hideChatMessage", translation = "config.skycubed.hideChatMessage")
-    @Comment("", translation = "config.skycubed.hideChatMessage.desc")
+    @ConfigEntry(id = "hideChatMessage", translation = "config.skycubed.notification.hideChatMessage")
+    @Comment("", translation = "config.skycubed.notification.hideChatMessage.desc")
     var hideMessage: Boolean = false,
 
     @ConfigOption.Separator(value = "Toast Options", description = "Options for toast notifications.")
 
-    @ConfigEntry(id = "showAsToast", translation = "config.skycubed.showAsToast")
-    @Comment("", translation = "config.skycubed.showAsToast.desc")
+    @ConfigEntry(id = "showAsToast", translation = "config.skycubed.notification.showAsToast")
+    @Comment("", translation = "config.skycubed.notification.showAsToast.desc")
     var showAsToast: Boolean = false,
 
-    @ConfigEntry(id = "toastDuration", translation = "config.skycubed.toastDuration")
-    @Comment("", translation = "config.skycubed.toastDuration.desc")
+    @ConfigEntry(id = "toastDuration", translation = "config.skycubed.notification.toastDuration")
+    @Comment("", translation = "config.skycubed.notification.toastDuration.desc")
     @ConfigOption.Range(min = 0.0, max = 30000.0)
     @ConfigOption.Slider
     var toastDuration: Int = 5000,
