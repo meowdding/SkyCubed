955c59e3cb8376b4ba13c57d04fd3479c7eef0b3
Mona <59416038+meowora@users.noreply.github.com>
2025-05-18 12:37:56 +0200

    feat: add more info to tab list (#36)

--- src/client/kotlin/tech/thatgravyboat/skycubed/features/tablist/CompactTablist.kt
+++ src/client/kotlin/tech/thatgravyboat/skycubed/features/tablist/CompactTablist.kt
@@ -17,6 +17,7 @@ import tech.thatgravyboat.skyblockapi.api.events.info.TabListHeaderFooterChangeE
 import tech.thatgravyboat.skyblockapi.api.location.LocationAPI
 import tech.thatgravyboat.skyblockapi.api.profile.effects.EffectsAPI
 import tech.thatgravyboat.skyblockapi.api.profile.friends.FriendsAPI
+import tech.thatgravyboat.skyblockapi.api.profile.party.PartyAPI
 import tech.thatgravyboat.skyblockapi.helpers.McClient
 import tech.thatgravyboat.skyblockapi.helpers.McFont
 import tech.thatgravyboat.skyblockapi.utils.extentions.chunked
@@ -46,7 +47,7 @@ private data class Line(
     var face: PlayerSkin? = null,
     var playerName: String? = null,
     var skyblockLevel: Int? = null,
-    var contribEmblem: String? = null
+    var extraEmblems: MutableList<Component> = mutableListOf(),
 ) {
     val string: String get() = component.string
     val stripped: String get() = component.stripped
@@ -126,7 +127,7 @@ object CompactTablist {
                     listOfNotNull(
                         Displays.face({ face.texture }),
                         Displays.text(line.component),
-                        line.contribEmblem?.let { Displays.text(it) },
+                        *line.extraEmblems.map { Displays.text(it) }.toTypedArray(),
                     ).toRow(3)
                 } ?: Displays.text(line.component)
             }.toColumn()
@@ -172,7 +173,29 @@ object CompactTablist {
             playerName = name
             face = player?.skin
             skyblockLevel = level.toInt()
-            contributor?.symbol?.let { contribEmblem = it }
+            // party > coop > friends > guild
+            // Todo add apis for this in sbapi
+            val isCoop = false
+            val isGuild = false
+            fun addExtraTag(emblem: String, color: Int) {
+                extraEmblems.add(Text.of(emblem) { this.color = color })
+            }
+            when {
+                PartyAPI.members.any { it.name == playerName } -> addExtraTag(
+                    "ᴘ",
+                    if (PartyAPI.leader?.name == playerName) {
+                        TextColor.GOLD
+                    } else {
+                        TextColor.BLUE
+                    },
+                )
+
+                isCoop -> addExtraTag("ᴄ", TextColor.AQUA)
+                FriendsAPI.friends.any { it.name == playerName } -> addExtraTag("ꜰ", TextColor.GREEN)
+                isGuild -> addExtraTag("ɢ", TextColor.DARK_GREEN)
+                else -> {}
+            }
+            contributor?.symbol?.let { extraEmblems.add(Text.of(it)) }
         }
         return this
     }
@@ -213,7 +236,7 @@ object CompactTablist {
             label: String,
             duration: Duration,
             activeColor: Int,
-            inactiveText: String = ": Inactive"
+            inactiveText: String = ": Inactive",
         ) = Text.join(
             Text.of(label) {
                 this.color = activeColor
@@ -221,8 +244,8 @@ object CompactTablist {
             },
             Text.of(
                 if (duration.inWholeSeconds > 0) ": ${duration.formatReadableTime(DurationUnit.DAYS, 2)}"
-                else inactiveText
-            ).withColor(TextColor.GRAY)
+                else inactiveText,
+            ).withColor(TextColor.GRAY),
         ).toLine()
 
         if (boosterCookieInFooter) {
@@ -244,16 +267,19 @@ object CompactTablist {
                     Text.of(SpookyFestivalAPI.purpleCandy.toString()).withColor(TextColor.DARK_PURPLE),
                     Text.of(" (").withColor(TextColor.GRAY),
                     Text.of(SpookyFestivalAPI.points.toString()).withColor(TextColor.GOLD),
-                    Text.of(")").withColor(TextColor.GRAY)
-                ).toLine()
+                    Text.of(")").withColor(TextColor.GRAY),
+                ).toLine(),
             )
         }
 
         if (size > 1) {
-            add(0, Text.of("Other:") {
-                this.color = TextColor.YELLOW
-                this.bold = true
-            }.toLine())
+            add(
+                0,
+                Text.of("Other:") {
+                    this.color = TextColor.YELLOW
+                    this.bold = true
+                }.toLine(),
+            )
         }
     }
 
