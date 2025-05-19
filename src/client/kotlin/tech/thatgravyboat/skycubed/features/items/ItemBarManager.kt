1f4a54f3301eb20b2b714d353cc538e75f2eeb8a
Gravy Boat <gravy@thatgravyboat.tech>
2025-05-19 14:00:40 -0230

    feat: item bar for the other thigns :3

--- src/client/kotlin/tech/thatgravyboat/skycubed/features/items/ItemBarManager.kt
+++ src/client/kotlin/tech/thatgravyboat/skycubed/features/items/ItemBarManager.kt
@@ -9,6 +9,8 @@ import tech.thatgravyboat.skyblockapi.api.events.render.RenderItemBarEvent
 import tech.thatgravyboat.skyblockapi.utils.text.TextColor
 import tech.thatgravyboat.skycubed.config.items.ItemsConfig
 
+private const val THREE_HUNDRED_HOURS = 1080000f
+
 @Module
 object ItemBarManager {
 
@@ -30,8 +32,16 @@ object ItemBarManager {
             event.color = Mth.hsvToRgb(event.percent / 3.0F, 1.0F, 1.0F)
         }
         event.item.getData(DataTypes.SECONDS_HELD)?.let {
-            event.percent = it / 1_000_000f
+            event.percent = it / THREE_HUNDRED_HOURS
             event.color = TextColor.GOLD
+        }
+        event.item.getData(DataTypes.BOTTLE_OF_JYRRE_SECONDS)?.let {
+            event.percent = it / THREE_HUNDRED_HOURS
+            event.color = TextColor.BLUE
+        }
+        event.item.getData(DataTypes.RIFT_DISCRITE_SECONDS)?.let {
+            event.percent = it / THREE_HUNDRED_HOURS
+            event.color = TextColor.RED
         }
     }
 }
