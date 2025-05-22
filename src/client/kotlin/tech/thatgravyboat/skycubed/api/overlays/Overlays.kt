61e8b7d729707e46848767799b5cbbc089830596
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-05-22 10:28:49 +0200

    refactor: idk sophie send me this
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skycubed/api/overlays/Overlays.kt
+++ src/client/kotlin/tech/thatgravyboat/skycubed/api/overlays/Overlays.kt
@@ -2,8 +2,10 @@ package tech.thatgravyboat.skycubed.api.overlays
 
 import me.owdding.ktmodules.Module
 import net.minecraft.ChatFormatting
+import net.minecraft.client.gui.components.Tooltip
 import net.minecraft.client.gui.screens.ChatScreen
 import net.minecraft.client.gui.screens.Screen
+import net.minecraft.client.gui.screens.inventory.tooltip.DefaultTooltipPositioner
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyOnSkyBlock
 import tech.thatgravyboat.skyblockapi.api.events.misc.RegisterCommandsEvent
@@ -58,35 +60,35 @@ object Overlays {
         overlays.forEach {
             if (!it.enabled) return@forEach
             val (x, y) = it.position
-
-            val rect = it.editBounds * it.position.scale
-            var hovered = isOverlayScreen(screen, mouseX.toInt(), mouseY.toInt()) && rect.contains(mouseX.toInt(), mouseY.toInt())
-
-            if (hovered) {
-                if (it.moveable) {
-                    screen!!.setTooltipForNextRenderPass(Text.multiline(
-                        it.name,
-                        CommonText.EMPTY,
-                        Text.translatable("ui.skycubed.overlay.edit"),
-                        Text.of("SkyCubed") {
-                            this.color = TextColor.BLUE
-                            this.withStyle(ChatFormatting.ITALIC)
-                        }
-                    ))
-                } else {
-                    screen!!.setTooltipForNextRenderPass(it.name)
-                }
-            }
-
             graphics.pushPop {
                 translate(x.toFloat(), y.toFloat(), 0f)
                 scale(it.position.scale, it.position.scale, 1f)
                 it.render(graphics, mouseX.toInt(), mouseY.toInt())
             }
 
-            if (hovered) {
+            val rect = it.editBounds * it.position.scale
+
+            if (isOverlayScreen(screen, mouseX.toInt(), mouseY.toInt()) && rect.contains(mouseX.toInt(), mouseY.toInt())) {
                 graphics.fill(rect.x, rect.y, rect.right, rect.bottom, 0x50000000)
                 graphics.renderOutline(rect.x - 1, rect.y - 1, rect.width + 2, rect.height + 2, 0xFFFFFFFF.toInt())
+                if (it.moveable) {
+                    val text = Tooltip.splitTooltip(
+                        McClient.self,
+                        Text.multiline(
+                            it.name,
+                            CommonText.EMPTY,
+                            Text.translatable("ui.skycubed.overlay.edit"),
+                            Text.of("SkyCubed") {
+                                this.color = TextColor.BLUE
+                                this.withStyle(ChatFormatting.ITALIC)
+                            },
+                        ),
+                    )
+                    screen!!.setTooltipForNextRenderPass(text, DefaultTooltipPositioner.INSTANCE, false)
+                } else {
+                    val text = Tooltip.splitTooltip(McClient.self, it.name)
+                    screen!!.setTooltipForNextRenderPass(text, DefaultTooltipPositioner.INSTANCE, false)
+                }
             }
         }
     }
