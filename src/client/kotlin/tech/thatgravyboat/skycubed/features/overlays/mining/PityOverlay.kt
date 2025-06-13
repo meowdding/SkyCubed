0ad88ca2a7fbcaa95dae935441135ec3168ad1f2
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-06-13 12:57:07 +0200

    woawzers
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/client/kotlin/tech/thatgravyboat/skycubed/features/overlays/mining/PityOverlay.kt
+++ src/client/kotlin/tech/thatgravyboat/skycubed/features/overlays/mining/PityOverlay.kt
@@ -1,36 +1,63 @@
 package tech.thatgravyboat.skycubed.features.overlays.mining
 
+import me.owdding.ktmodules.Module
 import me.owdding.lib.builder.DisplayFactory
+import me.owdding.lib.displays.Alignment
+import me.owdding.lib.displays.Displays
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.network.chat.Component
-import tech.thatgravyboat.skyblockapi.api.area.mining.GlaciteAPI
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.Blocks
 import tech.thatgravyboat.skyblockapi.api.area.mining.MiningBlock
 import tech.thatgravyboat.skyblockapi.api.area.mining.MiningBlockFamily
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyIn
 import tech.thatgravyboat.skyblockapi.api.events.level.MiningBlockMinedEvent
-import tech.thatgravyboat.skyblockapi.api.events.location.IslandChangeEvent
+import tech.thatgravyboat.skyblockapi.api.events.location.mineshaft.MineshaftFoundEvent
+import tech.thatgravyboat.skyblockapi.api.location.SkyBlockArea
+import tech.thatgravyboat.skyblockapi.api.location.SkyBlockAreas
 import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
+import tech.thatgravyboat.skyblockapi.utils.extentions.toFormattedString
 import tech.thatgravyboat.skyblockapi.utils.text.Text
 import tech.thatgravyboat.skycubed.api.overlays.Overlay
+import tech.thatgravyboat.skycubed.api.overlays.RegisterOverlay
 import tech.thatgravyboat.skycubed.config.overlays.OverlayPositions
 import tech.thatgravyboat.skycubed.config.overlays.Position
 import tech.thatgravyboat.skycubed.utils.CachedValue
+import kotlin.math.ceil
 import kotlin.time.Duration.Companion.seconds
 
 const val PITY_AMOUNT = 2000
 
+@Module
+@RegisterOverlay
 object PityOverlay : Overlay {
     override val name: Component = Text.of("Pity Overlay")
     override val position: Position get() = OverlayPositions.pity
     override val bounds: Pair<Int, Int> get() = display.get().getWidth() to display.get().getHeight()
+    override val enabled: Boolean get() = inTunnels
 
     var currentPity = PITY_AMOUNT
         private set
 
+    val inTunnels
+        get() = SkyBlockIsland.DWARVEN_MINES.inIsland() && SkyBlockArea.inAnyArea(
+            SkyBlockAreas.GLACITE_TUNNELS,
+            SkyBlockAreas.GREAT_LAKE,
+            SkyBlockAreas.BASECAMP,
+            SkyBlockAreas.FOSSIL_RESEARCH,
+        )
+
     private val display = CachedValue(1.seconds) {
         DisplayFactory.vertical {
             string("Current Pity: $currentPity")
+            PityBlock.entries.reversed().forEach { entry ->
+                val leftToMine = ceil(currentPity / entry.score.toDouble())
+                horizontal(alignment = Alignment.CENTER) {
+                    entry.icons.forEach { display(Displays.item(it)) }
+                    string(": ${leftToMine.toFormattedString()}")
+                }
+            }
         }
     }
 
@@ -41,7 +68,7 @@ object PityOverlay : Overlay {
     @Subscription
     @OnlyIn(SkyBlockIsland.DWARVEN_MINES)
     fun onBlockMined(event: MiningBlockMinedEvent) {
-        if (!GlaciteAPI.inGlaciteTunnels()) return
+        if (!inTunnels) return
 
         val score = PityBlock.fromBlock(event.block)?.score ?: return
 
@@ -49,18 +76,30 @@ object PityOverlay : Overlay {
     }
 
     @Subscription
-    @OnlyIn(SkyBlockIsland.MINESHAFT)
-    fun onIslandSwitch(event: IslandChangeEvent) {
+    fun onIslandSwitch(event: MineshaftFoundEvent) {
         currentPity = PITY_AMOUNT
     }
 
     enum class PityBlock(
         val blocks: List<MiningBlock>,
         val score: Int,
+        val icons: List<Block> = blocks.flatMap { it.blocks },
     ) {
-        GLACIAL_BLOCKS(MiningBlock.entries.filter { it.family in listOf(MiningBlockFamily.GEMSTONES, MiningBlockFamily.GLACITE) }, 4),
-        MITHRIL_BLOCKS(MiningBlock.entries.filter { it.family == MiningBlockFamily.MITHRIL }, 2),
-        TITANIUM(listOf(MiningBlock.TITANIUM), 8);
+        TITANIUM(listOf(MiningBlock.TITANIUM), 8),
+        GLACIAL_BLOCKS(
+            MiningBlock.entries.filter { it.family in listOf(MiningBlockFamily.GEMSTONES, MiningBlockFamily.GLACITE) },
+            4,
+            listOf(Blocks.RED_STAINED_GLASS, Blocks.SMOOTH_RED_SANDSTONE, Blocks.CLAY, Blocks.PACKED_ICE),
+        ),
+        MITHRIL_BLOCKS(
+            MiningBlock.entries.filter { it.family == MiningBlockFamily.MITHRIL },
+            2,
+            listOf(Blocks.LIGHT_BLUE_WOOL),
+        ),
+        ;
+
+        operator fun component1() = blocks
+        operator fun component2() = score
 
         companion object {
             fun fromBlock(block: MiningBlock) = PityBlock.entries.firstOrNull { block in it.blocks }
