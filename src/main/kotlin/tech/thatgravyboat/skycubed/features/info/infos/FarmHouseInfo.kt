b043ee6acab5687366a0ff7ac08cfef8a85ad96b
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-07-29 14:41:55 +0200

    refactor: only show medals info in garden barn
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skycubed/features/info/infos/FarmHouseInfo.kt
+++ src/main/kotlin/tech/thatgravyboat/skycubed/features/info/infos/FarmHouseInfo.kt
@@ -1,9 +1,13 @@
 package tech.thatgravyboat.skycubed.features.info.infos
 
 import me.owdding.lib.builder.DisplayFactory
+import net.minecraft.world.phys.AABB
 import tech.thatgravyboat.skyblockapi.api.area.hub.FarmhouseAPI
+import tech.thatgravyboat.skyblockapi.api.location.LocationAPI
 import tech.thatgravyboat.skyblockapi.api.location.SkyBlockAreas
 import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
+import tech.thatgravyboat.skyblockapi.helpers.McPlayer
+import tech.thatgravyboat.skyblockapi.helpers.McPlayer.contains
 import tech.thatgravyboat.skyblockapi.utils.extentions.toFormattedString
 import tech.thatgravyboat.skyblockapi.utils.text.TextColor
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
@@ -14,10 +18,14 @@ import tech.thatgravyboat.skycubed.features.info.RegisterInfoOverlay
 
 @RegisterInfoOverlay
 object FarmHouseInfo : InfoProvider {
+    private val barnAABB = AABB(-48.0, 60.0, -48.0, 47.0, 120.0, 47.0)
     override val location = InfoLocation.BOTTOM_LEFT
 
-    override val areas = listOf(SkyBlockAreas.FARMHOUSE, SkyBlockAreas.GARDEN)
-    override val islands = listOf(SkyBlockIsland.HUB, SkyBlockIsland.GARDEN)
+    override fun shouldDisplay() = when (LocationAPI.island) {
+        SkyBlockIsland.HUB -> SkyBlockAreas.FARMHOUSE.inArea()
+        SkyBlockIsland.GARDEN -> McPlayer in barnAABB
+        else -> false
+    }
 
     override fun getDisplay() = DisplayFactory.horizontal {
         display(getIconDisplay(SkyCubed.id("info/icons/gold")))
