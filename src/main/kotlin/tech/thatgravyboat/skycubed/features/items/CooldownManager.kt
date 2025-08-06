481ca3ac6b31a006d249135ec07b8253f99e5a57
Gravy Boat <gravy@thatgravyboat.tech>
2025-08-06 11:47:16 -0230

    fix: remove old monkey Tree Capitator cooldown

--- src/main/kotlin/tech/thatgravyboat/skycubed/features/items/CooldownManager.kt
+++ src/main/kotlin/tech/thatgravyboat/skycubed/features/items/CooldownManager.kt
@@ -2,13 +2,10 @@ package tech.thatgravyboat.skycubed.features.items
 
 import me.owdding.ktmodules.Module
 import net.minecraft.world.item.ItemStack
-import tech.thatgravyboat.skyblockapi.api.data.SkyBlockRarity
 import tech.thatgravyboat.skyblockapi.api.datatype.DataTypes
 import tech.thatgravyboat.skyblockapi.api.datatype.getData
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
-import tech.thatgravyboat.skyblockapi.api.events.level.BlockMinedEvent
 import tech.thatgravyboat.skyblockapi.api.events.level.RightClickEvent
-import tech.thatgravyboat.skyblockapi.api.profile.PetsAPI
 import tech.thatgravyboat.skyblockapi.helpers.McPlayer
 import tech.thatgravyboat.skycubed.config.items.ItemsConfig
 
@@ -27,22 +24,9 @@ object CooldownManager {
         }
     }
 
-    @Subscription
-    fun onBlockMined(event: BlockMinedEvent) {
-        val id = McPlayer.heldItem.getData(DataTypes.ID)
-        if (id == "TREECAPITATOR_AXE" || id == "JUNGLE_AXE") {
-            var reduction = 0.0
-            if (PetsAPI.pet.equals("monkey", true) && PetsAPI.rarity != null && PetsAPI.rarity!! >= SkyBlockRarity.LEGENDARY) {
-                reduction = PetsAPI.level * 0.005
-            }
-            addCooldown("TreeCapitator", 2000 - (2000 * reduction).toLong())
-        }
-    }
-
     private fun getCooldownId(stack: ItemStack): String? {
         return when (stack.getData(DataTypes.ID)) {
             "GRAPPLING_HOOK" -> "Grappling Hook"
-            "TREECAPITATOR_AXE", "JUNGLE_AXE" -> "TreeCapitator"
             else -> stack.getData(DataTypes.COOLDOWN_ABILITY)?.first
         }
     }
