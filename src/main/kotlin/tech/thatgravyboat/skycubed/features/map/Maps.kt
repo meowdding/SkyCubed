7c0a2de0bebc9602dfa385036e22c7580f162e85
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-08-13 17:38:41 +0200

    feat: /skycubed map
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skycubed/features/map/Maps.kt
+++ src/main/kotlin/tech/thatgravyboat/skycubed/features/map/Maps.kt
@@ -12,6 +12,7 @@ import net.minecraft.client.KeyMapping
 import net.minecraft.core.BlockPos
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.location.IslandChangeEvent
+import tech.thatgravyboat.skyblockapi.api.events.misc.RegisterCommandsEvent
 import tech.thatgravyboat.skyblockapi.api.events.time.TickEvent
 import tech.thatgravyboat.skyblockapi.api.location.LocationAPI
 import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
@@ -77,6 +78,13 @@ object Maps {
     @Subscription
     fun onTick(event: TickEvent) {
         if (MAP_KEYBIND.consumeClick()) {
+            McClient.setScreen(MapScreen())
+        }
+    }
+
+    @Subscription
+    fun onCommand(event: RegisterCommandsEvent) {
+        event.registerWithCallback("skycubed map") {
             McClient.setScreen(MapScreen())
         }
     }
