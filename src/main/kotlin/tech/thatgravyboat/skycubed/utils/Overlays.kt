df72743b0d3c65d8741a2677a2d91cb746538fad
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-11-12 15:43:41 +0100

    maybe start
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skycubed/utils/Overlays.kt
+++ src/main/kotlin/tech/thatgravyboat/skycubed/utils/Overlays.kt
@@ -2,6 +2,7 @@ package tech.thatgravyboat.skycubed.utils
 
 import me.owdding.ktmodules.AutoCollect
 import me.owdding.lib.overlays.Overlay
+import net.minecraft.client.gui.GuiGraphics
 import tech.thatgravyboat.skycubed.SkyCubed
 
 @Retention(AnnotationRetention.SOURCE)
@@ -11,4 +12,23 @@ annotation class RegisterOverlay
 
 interface SkyCubedOverlay : Overlay {
     override val modId: String get() = SkyCubed.MOD_ID
+    val background: OverlayBackground get() = OverlayBackground.TEXTURED
+
+    override fun render(graphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTicks: Float) {
+        when (background) {
+            OverlayBackground.TEXTURED -> {}
+            OverlayBackground.COLORED -> {}
+            OverlayBackground.NO_BACKGROUND -> {}
+        }
+        renderWithBackground(graphics, mouseX, mouseY, partialTicks)
+    }
+
+    fun renderWithBackground(graphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTicks: Float) = renderWithBackground(graphics, mouseX, mouseY)
+    fun renderWithBackground(graphics: GuiGraphics, mouseX: Int, mouseY: Int)
+}
+
+enum class OverlayBackground {
+    TEXTURED,
+    COLORED,
+    NO_BACKGROUND;
 }
