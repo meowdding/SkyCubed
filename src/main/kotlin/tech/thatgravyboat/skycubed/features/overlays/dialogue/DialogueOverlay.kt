7b90dfe6d14927d40a6e4bac05aa9eb0ce397cca
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-12-06 21:36:22 +0100

    idek more stuff
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skycubed/features/overlays/dialogue/DialogueOverlay.kt
+++ src/main/kotlin/tech/thatgravyboat/skycubed/features/overlays/dialogue/DialogueOverlay.kt
@@ -1,23 +1,19 @@
 package tech.thatgravyboat.skycubed.features.overlays.dialogue
 
-import com.google.gson.JsonParser
-import com.mojang.blaze3d.platform.InputConstants
 import me.owdding.ktmodules.Module
 import me.owdding.lib.displays.*
 import me.owdding.lib.overlays.ConfigPosition
 import me.owdding.lib.overlays.EditableProperty
+import me.owdding.lib.utils.KeyboardInputs
 import me.owdding.lib.utils.keys
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.gui.screens.ChatScreen
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
 import net.minecraft.network.chat.Component
-import net.minecraft.network.chat.ComponentSerialization
-import tech.thatgravyboat.skyblockapi.api.SkyBlockAPI
 import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
 import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyOnSkyBlock
 import tech.thatgravyboat.skyblockapi.api.events.chat.ChatReceivedEvent
 import tech.thatgravyboat.skyblockapi.api.events.hypixel.ServerChangeEvent
-import tech.thatgravyboat.skyblockapi.api.events.misc.RegisterCommandsEvent
 import tech.thatgravyboat.skyblockapi.api.events.render.RenderScreenForegroundEvent
 import tech.thatgravyboat.skyblockapi.api.events.screen.ContainerInitializedEvent
 import tech.thatgravyboat.skyblockapi.api.events.time.TickEvent
@@ -26,14 +22,13 @@ import tech.thatgravyboat.skyblockapi.helpers.McScreen
 import tech.thatgravyboat.skyblockapi.utils.extentions.left
 import tech.thatgravyboat.skyblockapi.utils.extentions.scissor
 import tech.thatgravyboat.skyblockapi.utils.extentions.translated
-import tech.thatgravyboat.skyblockapi.utils.json.Json.toData
 import tech.thatgravyboat.skyblockapi.utils.regex.component.ComponentMatchResult
 import tech.thatgravyboat.skyblockapi.utils.regex.component.ComponentRegex
 import tech.thatgravyboat.skyblockapi.utils.regex.component.match
 import tech.thatgravyboat.skyblockapi.utils.text.Text
-import tech.thatgravyboat.skyblockapi.utils.text.Text.send
 import tech.thatgravyboat.skyblockapi.utils.text.TextColor
 import tech.thatgravyboat.skyblockapi.utils.text.TextProperties.stripped
+import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.command
 import tech.thatgravyboat.skycubed.config.overlays.NpcOverlayConfig
 import tech.thatgravyboat.skycubed.utils.BackgroundLessSkyCubedOverlay
 import tech.thatgravyboat.skycubed.utils.RegisterOverlay
@@ -45,14 +40,17 @@ import kotlin.math.max
 object DialogueOverlay : BackgroundLessSkyCubedOverlay {
 
     private val messageRegex = ComponentRegex("\\[NPC] (?<name>[^:]+): (?<message>.+)")
-    private val selectAnOptionRegex = ComponentRegex(".*(?:Select|Click) an option: (?<options>.+)")
+    private val selectAnOptionRegex = listOf(
+        ComponentRegex(".*(?:Select|Click) an option: (?<options>.+)"),
+        ComponentRegex("\\nAccept the trapper's task to hunt the animal\\?\\nClick an option: (?<options>.+)"),
+    )
     private val optionRegex = ComponentRegex("\\[(?<option>.*?)]")
     private val wordRegex = Regex("\\s+")
 
     private val queue = mutableListOf<Pair<Component, Component>>()
     private var nextCheck = 0L
 
-    private var options: List<Component> = emptyList()
+    private var options: List<Option> = emptyList()
 
     private var displayedOptions = false
     private var hudOverlayDisplay: Display = Displays.empty()
@@ -81,7 +79,13 @@ object DialogueOverlay : BackgroundLessSkyCubedOverlay {
             if (config.hideChatMessage) event.cancel()
         }
         selectAnOptionRegex.match(event.component, "options") { (optionsComponent) ->
-            options = optionRegex.findAll(optionsComponent).mapNotNull { it["option"] }.toList()
+            options = optionRegex.findAll(optionsComponent).mapNotNull { it["option"] }.mapIndexed { index, option ->
+                Option(
+                    component = option,
+                    keys = keys { 49 + index },
+                    command = option.command ?: "",
+                )
+            }
             if (config.hideSelectAnOptionMessage) event.cancel()
         }
     }
@@ -97,14 +101,6 @@ object DialogueOverlay : BackgroundLessSkyCubedOverlay {
     @OnlyOnSkyBlock
     fun onInventoryInit(event: ContainerInitializedEvent) {
         containerLeftPos = event.screen.left
-    }
-
-    val yesKeys = keys {
-        InputConstants.KEY_1
-    }
-
-    val noKeys = keys {
-        InputConstants.KEY_2
     }
 
     @Subscription
@@ -127,21 +123,21 @@ object DialogueOverlay : BackgroundLessSkyCubedOverlay {
 
                 nextCheck = System.currentTimeMillis() + calculateDisplayDuration(message)
 
-                createMainDisplay(name, message, npc, McClient.window.guiScaledWidth / 3)?.let { hudOverlayDisplay = it }
-                createMainDisplay(name, message, npc, inventoryOverlayWidth - 30)?.let { inventoryOverlayDisplay = it }
+                hudOverlayDisplay = createMainDisplay(name, message, npc, McClient.window.guiScaledWidth / 3)
+                inventoryOverlayDisplay = createMainDisplay(name, message, npc, inventoryOverlayWidth - 30)
             }
         }
 
-        /*val (yesCommand, noCommand) = yesNo ?: return
-        val isYes = yesKeys.isDown()
-        val isNo = noKeys.isDown()
-
-        val command = if (isYes) yesCommand else if (isNo) noCommand else return
-        McClient.sendCommand(command.removePrefix("/"))
-        reset()*/
+        options.forEach { (_, keys, command) ->
+            if (keys.isDown()) {
+                McClient.sendCommand(command.removePrefix("/"))
+                reset()
+                return
+            }
+        }
     }
 
-    private fun createMainDisplay(name: Component, message: Component, npc: DialogueNpc, maxWidth: Int): Display? {
+    private fun createMainDisplay(name: Component, message: Component, npc: DialogueNpc, maxWidth: Int): Display {
         val entity = DialogueEntities.get(name.stripped, npc)
         val npcNameDisplay = Displays.background(
             SkyCubedTextures.backgroundBox,
@@ -178,12 +174,10 @@ object DialogueOverlay : BackgroundLessSkyCubedOverlay {
         displayedOptions = true
         nextCheck = System.currentTimeMillis() + displayActionDuration
 
-        val yesNoDisplay = options.mapIndexed { index, component ->
+        val yesNoDisplay = options.mapIndexed { index, (component) ->
             val text = Text.join(Text.of("${index + 1}. ").withColor(TextColor.GRAY), component)
             Displays.background(SkyCubedTextures.backgroundBox, Displays.text(text).withPadding(5))
         }.toColumn(5, Alignment.START)
-
-        println("Created options display with options: $options")
 
         return object : Display {
             private val main = hudOverlayDisplay
@@ -238,82 +232,9 @@ object DialogueOverlay : BackgroundLessSkyCubedOverlay {
     private fun ComponentRegex.findAll(component: Component) = regex().findAll(component.stripped).map { ComponentMatchResult(component, it) }.toList()
 
 
-    @Subscription
-    fun command(event: RegisterCommandsEvent) {
-        event.registerWithCallback("skycubed npc") {
-            val message = """
-                {
-                  "text": "",
-                  "extra": [
-                    {
-                      "text": "§e[NPC] §6Royal Guard§f: ",
-                      "strikethrough": false
-                    },
-                    {
-                      "text": "§fGreat treasures lie ahead, but at what cost?",
-                      "strikethrough": false
-                    }
-                  ]
-                }
-            """.trimIndent()
-            val options = """
-                {
-                  "text": "§eSelect an option: ",
-                  "extra": [
-                    {
-                      "text": "§a[Sure...] ",
-                      "hover_event": {
-                        "value": {
-                          "text": "Click to select!",
-                          "strikethrough": false,
-                          "obfuscated": false,
-                          "bold": false,
-                          "italic": false,
-                          "underlined": false,
-                          "color": "#FFFF55"
-                        },
-                        "action": "show_text"
-                      },
-                      "strikethrough": false,
-                      "click_event": {
-                        "command": "/selectnpcoption molbert r_7_1",
-                        "action": "run_command"
-                      }
-                    },
-                    {
-                      "text": "§c[You look like a mole yourself] ",
-                      "hover_event": {
-                        "value": {
-                          "text": "Click to select!",
-                          "strikethrough": false,
-                          "obfuscated": false,
-                          "bold": false,
-                          "italic": false,
-                          "underlined": false,
-                          "color": "#FFFF55"
-                        },
-                        "action": "show_text"
-                      },
-                      "strikethrough": false,
-                      "click_event": {
-                        "command": "/selectnpcoption molbert r_7_2",
-                        "action": "run_command"
-                      }
-                    }
-                  ],
-                  "strikethrough": false
-                }
-            """.trimIndent()
-
-            fun send(json: String) {
-                val c = JsonParser.parseString(json).toData(ComponentSerialization.CODEC) ?: return
-                c.send()
-                ChatReceivedEvent.Pre(c).post(SkyBlockAPI.eventBus)
-                ChatReceivedEvent.Post(c).post(SkyBlockAPI.eventBus)
-            }
-
-            send(message)
-            send(options)
-        }
-    }
+    private data class Option(
+        val component: Component,
+        val keys: KeyboardInputs,
+        val command: String,
+    )
 }
