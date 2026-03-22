eea847f275e3280354ee72d2f5119d09f3bbd785
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-03-22 13:59:32 +0100

    png
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skycubed/features/info/infos/PestInfo.kt
+++ src/main/kotlin/tech/thatgravyboat/skycubed/features/info/infos/PestInfo.kt
@@ -1,10 +1,11 @@
 package tech.thatgravyboat.skycubed.features.info.infos
 
-import me.owdding.lib.displays.Display
-import me.owdding.lib.displays.Displays
+import me.owdding.lib.builder.DisplayFactory
 import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
 import tech.thatgravyboat.skyblockapi.api.profile.garden.PlotAPI
 import tech.thatgravyboat.skyblockapi.utils.text.TextColor
+import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
+import tech.thatgravyboat.skycubed.SkyCubed
 import tech.thatgravyboat.skycubed.features.info.InfoLocation
 import tech.thatgravyboat.skycubed.features.info.InfoProvider
 import tech.thatgravyboat.skycubed.features.info.RegisterInfoOverlay
@@ -13,8 +14,10 @@ import tech.thatgravyboat.skycubed.features.info.RegisterInfoOverlay
 object PestInfo : InfoProvider {
     override val location: InfoLocation = InfoLocation.BOTTOM_LEFT
 
-    override fun getDisplay(): Display = Displays.text("${PlotAPI.currentPestAmount} ൠ", color = { TextColor.DARK_GREEN.toUInt() })
+    override fun getDisplay() = DisplayFactory.horizontal {
+        display(getIconDisplay(SkyCubed.id("info/icons/pest")))
+        textDisplay("${PlotAPI.currentPestAmount}", shadow = true) { color = TextColor.DARK_GREEN }
+    }
 
     override fun shouldDisplay() = SkyBlockIsland.GARDEN.inIsland() && PlotAPI.currentPestAmount > 0
-
 }
