a4dbdbf7454d8d3f3ac8f2c00db3d893a1bc3d5e
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-04-03 12:42:44 +0200

    feat: sowdust + number formatting option
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skycubed/features/info/infos/CurrencyInfo.kt
+++ src/main/kotlin/tech/thatgravyboat/skycubed/features/info/infos/CurrencyInfo.kt
@@ -3,8 +3,7 @@ package tech.thatgravyboat.skycubed.features.info.infos
 import me.owdding.lib.builder.DisplayFactory
 import tech.thatgravyboat.skyblockapi.api.location.SkyBlockAreas
 import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
-import tech.thatgravyboat.skyblockapi.api.profile.CurrencyAPI
-import tech.thatgravyboat.skyblockapi.utils.extentions.toFormattedString
+import tech.thatgravyboat.skyblockapi.api.profile.currency.CurrencyAPI
 import tech.thatgravyboat.skyblockapi.utils.text.TextColor
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.color
 import tech.thatgravyboat.skycubed.SkyCubed
@@ -19,18 +18,21 @@ object CurrencyInfo : InfoProvider {
     override fun getDisplay() = DisplayFactory.horizontal {
         if (SkyBlockIsland.THE_RIFT.inIsland()) {
             display(getIconDisplay(SkyCubed.id("info/icons/rift/motes")))
-            textDisplay(CurrencyAPI.motes.toFormattedString(), shadow = true) { color = 0xFF55FF }
+            textDisplay(CurrencyAPI.motes.format(), shadow = true) { color = 0xFF55FF }
             return@horizontal
         }
         if (SkyBlockIsland.GARDEN.inIsland() or SkyBlockAreas.FARMHOUSE.inArea()) {
+            display(getIconDisplay(SkyCubed.id("info/icons/sowdust")))
+            textDisplay(CurrencyAPI.sowdust.format(), shadow = true) { color = TextColor.DARK_GREEN }
+
             display(getIconDisplay(SkyCubed.id("info/icons/bronze")))
-            textDisplay(CurrencyAPI.copper.toFormattedString(), shadow = true) { color = TextColor.RED }
+            textDisplay(CurrencyAPI.copper.format(), shadow = true) { color = TextColor.RED }
         }
 
         display(getIconDisplay(SkyCubed.id("info/icons/purse")))
-        textDisplay(CurrencyAPI.purse.toFormattedString(), shadow = true) { color = TextColor.GOLD }
+        textDisplay(CurrencyAPI.purse.format(), shadow = true) { color = TextColor.GOLD }
 
         display(getIconDisplay(SkyCubed.id("info/icons/bits")))
-        textDisplay(CurrencyAPI.bits.toFormattedString(), shadow = true) { color = TextColor.AQUA }
+        textDisplay(CurrencyAPI.bits.format(), shadow = true) { color = TextColor.AQUA }
     }
 }
