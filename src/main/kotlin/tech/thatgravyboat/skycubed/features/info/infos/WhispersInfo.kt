449e021c04b65ccef378212701ffcadd7793a56b
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-08-03 19:38:24 +0200

    feat: desert whispers in info overlay
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/tech/thatgravyboat/skycubed/features/info/infos/WhispersInfo.kt
+++ src/main/kotlin/tech/thatgravyboat/skycubed/features/info/infos/WhispersInfo.kt
@@ -1,6 +1,7 @@
 package tech.thatgravyboat.skycubed.features.info.infos
 
 import me.owdding.lib.builder.DisplayFactory
+import tech.thatgravyboat.skyblockapi.api.location.LocationAPI
 import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
 import tech.thatgravyboat.skyblockapi.api.profile.hotf.WhispersAPI
 import tech.thatgravyboat.skyblockapi.utils.text.TextColor
@@ -10,15 +11,19 @@ import tech.thatgravyboat.skycubed.features.info.InfoLocation
 import tech.thatgravyboat.skycubed.features.info.InfoProvider
 import tech.thatgravyboat.skycubed.features.info.RegisterInfoOverlay
 
-
 @RegisterInfoOverlay
 object WhispersInfo : InfoProvider {
     override val location = InfoLocation.BOTTOM_LEFT
 
-    override val islands: List<SkyBlockIsland> = listOf(SkyBlockIsland.GALATEA)
+    override val islands: List<SkyBlockIsland> = listOf(SkyBlockIsland.GALATEA, SkyBlockIsland.TORRHUS_CANYON)
 
     override fun getDisplay() = DisplayFactory.horizontal {
-        display(getIconDisplay(SkyCubed.id("info/icons/forest")))
-        textDisplay(WhispersAPI.forest.format(), shadow = true) { color = TextColor.DARK_AQUA }
+        val (icon, whispers) = when (LocationAPI.island) {
+            SkyBlockIsland.GALATEA -> SkyCubed.id("info/icons/forest") to (WhispersAPI.forest to TextColor.DARK_AQUA)
+            SkyBlockIsland.TORRHUS_CANYON -> SkyCubed.id("info/icons/desert") to (WhispersAPI.desert to TextColor.GOLD)
+            else -> return@horizontal
+        }
+        display(getIconDisplay(icon))
+        textDisplay(whispers.first.format(), shadow = true) { color = whispers.second }
     }
 }
